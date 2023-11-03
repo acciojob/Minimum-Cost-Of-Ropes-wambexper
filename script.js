57dbe25ca32145a866376284bd02cf398f109748
PETER WAMBEDDE <wambexpeter@gmail.com>
2023-11-03 15:57:10 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-function calculateMinCost(arr) {
+function calculateMinCost() {
   //your code here
-arr.sort((a,b)=>a-b)
+  let cost = 0;
 
-	let result=arr.reduce((a,b)=>{
-		return a+b;
-},a+b)
+	while (set.length > 1) {
+	  let fmin = Number.MAX_VALUE;
+	  let smin = Number.MAX_VALUE;
+	
+	  for (let i of set) {
+	    if (i < fmin) {
+	      smin = fmin;
+	      fmin = i;
+	    } else if (i < smin) {
+	      smin = i;
+	    }
+	  }
+	
+	  let opCost = fmin + smin;
+	  set.push(opCost);
+	  cost += opCost;
+	
+	  // Remove the first occurrence of fmin and smin from the array
+	  set.splice(set.indexOf(fmin), 1);
+	  set.splice(set.indexOf(smin), 1);
+	}
+	return cost;  
   
-  console.log(result)
 }  
